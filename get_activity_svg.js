37bf536a4c72d863b860fe8aca0e296ed079d017
felat.aslan <felataslan58@gmail.com>
2026-03-07 04:24:19 +0300

    feat: Replace favicon.ico with a new activity SVG icon and add an icon generation script.

---
+++ get_activity_svg.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs');
+const lucide = require('lucide');
+
+const activity = lucide.icons.Activity;
+// The default Activity icon definition
+console.log(activity);
